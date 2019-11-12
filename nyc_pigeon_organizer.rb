975ef04e35bcebe6c76244d92db02543baecc72c
Madeline Corman <madelinecorman@gmail.com>
2019-11-12 21:13:04 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -2,7 +2,7 @@ def nyc_pigeon_organizer(data)
   result = {}
   data.each do |key, value|
     value.each do |new_value, name|
-     names.each do |name|
+     name.each do |name|
        
        if !result[name]
          result[name] = {}
